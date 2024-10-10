6604191239434bf127fa99c2d2af6b1b631e2633
harold1735 <harold.eustaquio.a@uni.pe>
2024-10-09 22:33:01 -0600

    Main files: This .py will be executed by calling the functions in the other file

--- Alert Message/wsp_2.py
+++ Alert Message/wsp_2.py
@@ -8,9 +8,9 @@ while True:
     df = get_data(engine,"select * from last_update_T where nombre_alternativo  like 'AOC2'")
     df = clean_data(df)
     message_T(df,driver)
-    
+
     sleep(15)
-    
+
     engine = connect_db()
     df = get_data(engine,"select * from last_update_H where nombre_alternativo  like 'AOC2'")
     df = clean_data(df)
